940ae3912bafeeabc112ba7cc7719cfa7e0f91e8
Eduardo <eduardo.moraes@clicksoft.com.br>
2024-05-20 18:17:26 -0300

    chore: add fonts and theme

---
+++ src/theme/colors.ts
@@ -0,0 +1,13 @@
+export const colors = {
+  white: "#FFFFFF",
+  black: "#000000",
+
+  primaryZinc: {
+    900: "#111111",
+    700: "#1E1E1E",
+    500: "#141414",
+  },
+  primaryRed: {
+    900: "#D62B24",
+  },
+};
